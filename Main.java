d65a0913a48a9db4f06b5794670ba868a79382b6
khajavali20 <95695081+khajavali20@users.noreply.github.com>
2022-12-16 13:40:34 +0530

    bubble_sort code

---
+++ Main.java
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+    static void bubble(int arr[]){
+        int temp=0;
+        int len=arr.length;
+        for(int i=0;i<len-1;i++){
+            for(int j=0;j<len-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+            
+        }
+    }
+	public static void main(String[] args) {
+		int arr[]={5,3,4,1,2};
+		
+		System.out.println("Before Buuble Sort: ");
+		for(int i=0;i<arr.length;i++){
+		    System.out.print( arr[i]+" ");
+		    
+		}
+		System.out.println();
+		
+		bubble(arr);
+		System.out.println("After Bubble Sort: ");
+		for(int i=0;i<arr.length;i++){
+		    System.out.print(arr[i]+" ");
+		}
+	}
+}
